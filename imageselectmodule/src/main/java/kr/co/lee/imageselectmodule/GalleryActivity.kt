315f881ed32153e11cba0d6020574cc95725e61b
Chang Hee Lee <40291663+dhtmaks2540@users.noreply.github.com>
2022-04-17 23:36:52 +0900

    커스텀 이미지 선택 모듈

---
+++ imageselectmodule/src/main/java/kr/co/lee/imageselectmodule/GalleryActivity.kt
@@ -0,0 +1,17 @@
+package kr.co.lee.imageselectmodule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.lee.howlstargram_kotlin.base.BaseActivity
+import kr.co.lee.imageselectmodule.databinding.ActivityGalleryBinding
+
+class GalleryActivity : BaseActivity<ActivityGalleryBinding>(R.layout.activity_gallery) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.apply {
+
+        }
+    }
+}
