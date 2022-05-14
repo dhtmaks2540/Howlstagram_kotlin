2d7f457e9fa22ce6dd44c1118f3a376373a5a1f6
Chang Hee Lee <40291663+dhtmaks2540@users.noreply.github.com>
2022-05-14 22:53:58 +0900

    Content Parcelable 구현

--- app/src/main/java/kr/co/lee/howlstargram_kotlin/model/ContentDTO.kt
+++ app/src/main/java/kr/co/lee/howlstargram_kotlin/model/ContentDTO.kt
@@ -1,5 +1,8 @@
 package kr.co.lee.howlstargram_kotlin.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class ContentDTO(
     var explain: String? = null,
     var imageUrl: String? = null,
@@ -8,4 +11,36 @@ data class ContentDTO(
     var timestamp: Long = 0,
     var favoriteCount: Int = 0,
     var favorites: MutableMap<String, Boolean> = HashMap(),
-)
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readLong(),
+        parcel.readInt(),
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(explain)
+        parcel.writeString(imageUrl)
+        parcel.writeString(uid)
+        parcel.writeString(userId)
+        parcel.writeLong(timestamp)
+        parcel.writeInt(favoriteCount)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ContentDTO> {
+        override fun createFromParcel(parcel: Parcel): ContentDTO {
+            return ContentDTO(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ContentDTO?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
