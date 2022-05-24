f759fc274620e067349338e211af5186165d642f
Chang Hee Lee <40291663+dhtmaks2540@users.noreply.github.com>
2022-05-24 22:34:10 +0900

    좋아요 아이템, 유저 아이템 데이터 코드 수정

--- app/src/main/java/kr/co/lee/howlstargram_kotlin/model/FollowDTO.kt
+++
@@ -1,8 +0,0 @@
-package kr.co.lee.howlstargram_kotlin.model
-
-data class FollowDTO(
-    val followerCount: Int = 0,
-    val followers: Map<String, Boolean>,
-    val followingCount: Int = 0,
-    val followings: Map<String, Boolean>
-)
